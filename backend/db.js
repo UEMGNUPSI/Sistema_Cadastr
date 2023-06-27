e7a797ccecb54d0a21c8faba9b9f0c33eafcdd99
NupsiFrutal <nupsi.frutal@uemg.br>
2023-06-27 10:57:57 -0300

    colocando atributo tabela cliente

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,7 @@
 const config = {
     HOST: "localhost",
     USER: "postgres",
-    PASSWORD: "root",
+    PASSWORD: "ROOT",
     DB: "SG",
     dialect: "postgres",
     define: {
@@ -33,7 +33,54 @@ db.clientes = sequelize.define("Cliente", {
     type: Sequelize.INTEGER,
     autoIncrement: true,
     primaryKey: true
-  }
+  },
+  cli_data_cadastro: {
+    type: Sequelize.DATE
+  },
+  cli_data_cadastro: {
+    type: Sequelize.DATE
+  },
+  cli_nome: {
+    type: Sequelize.STRING
+  },
+  cli_cpf_cnpj: {
+    type: Sequelize.STRING
+  },
+  cli_data_nasc_abertura: {
+    type: Sequelize.DATE
+  },
+  cli_nome_social: {
+    type: Sequelize.STRING
+  }, 
+  cli_email: {
+    type: Sequelize.STRING},
+  cli_rg_inscr_estad:{
+    type: Sequelize.STRING},
+  cli_telefone:{
+    type: Sequelize.STRING},
+  cli_telefone2:{
+    type: Sequelize.STRING},
+  cli_celular:{
+    type: Sequelize.STRING},
+  cli_cep:{
+    type: Sequelize.INTEGER},   
+  cli_logradouro:{
+    type: Sequelize.STRING},
+  cli_numero:{
+    type: Sequelize.STRING},
+  cli_complemento:{
+    type: Sequelize.STRING},
+  cli_bairro:{
+    type: Sequelize.STRING},   
+  cli_cidade:{
+    type: Sequelize.STRING},   
+  cli_estado:{
+    type: Sequelize.STRING},   
+  cli_pais:{
+    type: Sequelize.STRING}, 
+  cli_observacao:{
+    type: Sequelize.STRING}
+  
 });
 
 

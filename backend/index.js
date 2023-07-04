f026b2c3c4d092b1c14e5b2dcdf0f961154975d3
UEMGNUPSI <nupsi.frutal@uemg.br>
2023-07-04 11:06:09 -0300

    rota criar cliente

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 
+const bodyParser = require ('body-parser');
+app.use(bodyParser.json());
+
 const db = require("./db");
 const { where } = require("sequelize");
 
@@ -8,8 +11,20 @@ app.get("/", (req, res) => {
     res.json({ message: 'Conectado na Base de Dados' });
 });
 
-app.get("/clientes", async (req, res) => {
-    res.json(await db.clientes.findAll());
+app.get("/Cliente", async (req, res) => {
+    res.json(await db.Cliente.findAll());
+})
+
+app.post("/Cliente", async (req, res) => {
+    try {
+        const {cli_data_cadastro, cli_nome, cli_cpf_cnpj, cli_data_nasc_abertura, cli_nome_social, cli_email, cli_rg_inscr_estad, cli_telefone, cli_telefone2, cli_celular, cli_cep, cli_logradouro, cli_numero, cli_complemento, cli_bairro, cli_cidade, cli_estado, cli_pais, cli_observacao} = req.body;
+
+        const novo_cliente = await db.Cliente.create ({cli_data_cadastro, cli_nome, cli_cpf_cnpj, cli_data_nasc_abertura, cli_nome_social, cli_email, cli_rg_inscr_estad, cli_telefone, cli_telefone2, cli_celular, cli_cep, cli_logradouro, cli_numero, cli_complemento, cli_bairro, cli_cidade, cli_estado, cli_pais, cli_observacao});
+       
+        res.status (201).json (novo_cliente);
+    } catch (error) {
+        res.status (500).json ({ error: 'Não foi possível criar um novo cliente.'});
+    }
 })
 
 app.get("/usuarios", async (req, res) => {

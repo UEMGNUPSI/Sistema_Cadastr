2c019ed2916c80e05b24c2c7f5553463f2632374
NupsiFrutal <nupsi.frutal@uemg.br>
2023-07-11 09:54:59 -0300

    Documentando o usuario no apidoc

--- backend/db.js
+++ backend/db.js
@@ -1,36 +1,4 @@
 
-/**
-* @api {get} /Cliente/:cli_pk_id Exibir informações do cliente
- * @apiName GetClientes
- * @apiGroup Clientes
- *
- * @apiParam {Integer_Primarykey_AutoIcrement} cli_pk_id identificação do cliente.
- * @apiSuccess {Date} cli_data_cadastro Data de cadastro do cliente.
- * @apiSuccess {String} cli_nome Nome do Cliente.
- * @apiSuccess {String} cli_cpf_cnpj CPF ou CNPJ do Cliente.
- * @apiSuccess {Date} cli_data_nasc_abertura Data de nascimento do cliente fisíco ou data de abertura do cliente juridíco.
- * @apiSuccess {String} cli_nome_social Nome Social do Cliente.
- * @apiSuccess {String} cli_rg_inscr_estad RG do cliente fisíco ou Incrição Estadual do cliente juridíco.
- * @apiSuccess {String} cli_email Email do Cliente.
- * @apiSuccess {String} cli_telefone Telefone do Cliente.
- * @apiSuccess {String} cli_telefone2 Telefone2 do Cliente.
- * @apiSuccess {String} cli_celular Celular do Cliente.
- * @apiSuccess {String} cli_cep CEP do Cliente.
- * @apiSuccess {String} cli_logradouro Logradouro do Cliente.
- * @apiSuccess {String} cli_numero Número do imovel do Cliente.
- * @apiSuccess {String} cli_cep CEP do Cliente.
- * @apiSuccess {String} cli_bairro Bairro do Cliente.
- * @apiSuccess {String} cli_cidade Cidade do Cliente.
- * @apiSuccess {String} cli_estado Estado do Cliente.
- * @apiSuccess {String} cli_pais País do Cliente.
- * @apiSuccess {String} cli_observacao Observação do Cliente.
-
- */
-/**
- * @api {post} /Cliente Criar um novo Cliente
- * @apiName PutClientes
- * @apiGroup Clientes
- */
 
 
 const config = {
--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,53 @@
+/**
+* @api {get} /Cliente/:cli_pk_id Exibir informações do cliente
+ * @apiName GetClientes
+ * @apiGroup Clientes
+ *
+ * @apiParam {Integer_Primarykey_AutoIcrement} cli_pk_id identificação do cliente.
+ * @apiSuccess {Date} cli_data_cadastro Data de cadastro do cliente.
+ * @apiSuccess {String} cli_nome Nome do Cliente.
+ * @apiSuccess {String} cli_cpf_cnpj CPF ou CNPJ do Cliente.
+ * @apiSuccess {Date} cli_data_nasc_abertura Data de nascimento do cliente fisíco ou data de abertura do cliente juridíco.
+ * @apiSuccess {String} cli_nome_social Nome Social do Cliente.
+ * @apiSuccess {String} cli_rg_inscr_estad RG do cliente fisíco ou Incrição Estadual do cliente juridíco.
+ * @apiSuccess {String} cli_email Email do Cliente.
+ * @apiSuccess {String} cli_telefone Telefone do Cliente.
+ * @apiSuccess {String} cli_telefone2 Telefone2 do Cliente.
+ * @apiSuccess {String} cli_celular Celular do Cliente.
+ * @apiSuccess {String} cli_cep CEP do Cliente.
+ * @apiSuccess {String} cli_logradouro Logradouro do Cliente.
+ * @apiSuccess {String} cli_numero Número do imovel do Cliente.
+ * @apiSuccess {String} cli_cep CEP do Cliente.
+ * @apiSuccess {String} cli_bairro Bairro do Cliente.
+ * @apiSuccess {String} cli_cidade Cidade do Cliente.
+ * @apiSuccess {String} cli_estado Estado do Cliente.
+ * @apiSuccess {String} cli_pais País do Cliente.
+ * @apiSuccess {String} cli_observacao Observação do Cliente.
+
+ */
+/**
+ * @api {post} /Cliente Criar um novo cliente
+ * @apiName PutClientes
+ * @apiGroup Clientes
+ */
+
+/**
+* @api {get} /usuarios/:usu_pk_id Exibir informações do usuário
+ * @apiName GetUsuário
+ * @apiGroup Usuários
+ *
+ * @apiParam {Integer_Primarykey_AutoIcrement} usu_pk_id identificação do Usuário.
+ * @apiSuccess {String} usu_nome Nome do Usuário.
+ * @apiSuccess {String} usu_senha Senha de acesso do Usuário.
+ * @apiSuccess {String} usu_email Email do Usuário.
+ * @apiSuccess {String} usu_grupo Grupo do Usuário.
+ * @apiSuccess {String} usu_ativo Status do Usuário.
+ */
+/**
+ * @api {post} /usuarios Criar um novo usuário
+ * @apiName PutUsuário
+ * @apiGroup Usuários
+ */
 const express = require("express");
 const app = express();
 
@@ -31,7 +81,7 @@ app.get("/usuarios", async (req, res) => {
     res.json(await db.usuarios.findAll());
 })
 
-app.get("/usuarios/:id", (req, res) => {
+app.get("/usuarios/:usu_pk_id", (req, res) => {
 
     const userId = req.params.id;
 

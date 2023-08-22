dad135e5d1bd3da6f3dc33f4bf585a36303e30b7
NupsiFrutal <nupsi.frutal@uemg.br>
2023-08-22 10:24:38 -0300

    alterando pf e pj

---
+++ frontend/FormularioCliente.js
@@ -0,0 +1,12 @@
+function troca1(){
+    var RGlabel = document.getElementById('RGlabel');
+    
+    RGlabel.innerHTML = 'RG';
+    };
+    
+    function troca2(){
+    var RGlabel = document.getElementById('RGlabel');
+    
+    RGlabel.innerHTML = 'IE';
+    };
+    
